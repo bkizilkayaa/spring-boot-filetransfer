0035f6da0b8bbade2a4f81f01ade5441c2982ca3
Burak Kizilkaya <kizilkayaburakk@hotmail.com>
2022-10-07 11:12:15 +0300

    Initial commit

---
+++ src/main/java/com/burakkizilkaya/multipart/filetransfer/FiletransferApplication.java
@@ -0,0 +1,13 @@
+package com.burakkizilkaya.multipart.filetransfer;
+
+import org.springframework.boot.SpringApplication;
+import org.springframework.boot.autoconfigure.SpringBootApplication;
+
+@SpringBootApplication
+public class FiletransferApplication {
+
+	public static void main(String[] args) {
+		SpringApplication.run(FiletransferApplication.class, args);
+	}
+
+}
